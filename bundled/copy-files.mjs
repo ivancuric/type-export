5884e4532ecb397251993613a4978289ecd65916
Ivan Čurić <ivan.curic@gmail.com>
2022-12-19 15:30:39 +0100

    Declaration maps WIP

--- bundled/copy-files.mjs
+++ bundled/copy-files.mjs
@@ -3,10 +3,12 @@
 import { $ } from "zx";
 
 try {
+  await $`rm -rf core`;
+  await $`rm -rf addon`;
   await $`mkdir -p core`;
   await $`mkdir -p addon`;
-  await $`cp -a ../core/dist core/dist`;
-  await $`cp -a ../core/types core/types`;
-  await $`cp -a ../addon/dist addon/dist`;
-  await $`cp -a ../addon/types addon/types`;
+  await $`cp -a ../core/dist/ core/dist/`;
+  await $`cp -a ../core/types/ core/types/`;
+  await $`cp -a ../addon/dist/ addon/dist/`;
+  await $`cp -a ../addon/types/ addon/types/`;
 } catch (error) {}
