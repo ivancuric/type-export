6ef84446e9b27866ae53caea9d14364770d7c561
Ivan Čurić <ivan.curic@gmail.com>
2022-12-19 11:34:28 +0100

    4 workspaces

---
+++ dependency/vite.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      emptyOutDir: false,
+      minify: false,
+      sourcemap: "inline",
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "dependency",
+        fileName: "dependency",
+      },
+    },
+  };
+});
