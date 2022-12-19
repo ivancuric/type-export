56b19bf66d5d0c9fc5a98b1d0fa02daa01d1a49b
Ivan Čurić <ivan.curic@gmail.com>
2022-12-19 14:57:41 +0100

    Working with nodenext imports

--- consumer/src/index.ts
+++ consumer/src/index.ts
@@ -1,4 +1,6 @@
-// import { coreValue } from "bundled";
-// import { addonValue } from "bundled/addon";
+import { coreValue } from "bundled";
+import { addonValue } from "bundled/addon";
 
 export {};
+
+console.log(addonValue, coreValue);
